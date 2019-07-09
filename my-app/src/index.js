6fa0ce779c134355aa9216511fc694d7970b73c1
tofu0308 <isisaka2000@gmail.com>
2019-07-09 23:58:53 +0900

    button click時にstateをXに書き換え再レンダー

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      value: null,
+    } ;
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button
+        className="square"
+        onClick = {() => {
+          this.setState({value: 'X'});
+          console.log(this.props.value);          
+        }}
+      >
+        {this.state.value}
       </button>
     );
   }
